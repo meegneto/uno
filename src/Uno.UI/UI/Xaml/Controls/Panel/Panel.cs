1bb5bb260614f7ac225adb7768a5ab7a63a00d2d
Jerome Laban <jerome.laban@nventive.com>
2023-06-19 16:43:42 -0400

    feat: WinUI StackPanel
    Co-authored-by: Martin Zikmund <martin@zikmund.dev>

--- src/Uno.UI/UI/Xaml/Controls/Panel/Panel.cs
+++ src/Uno.UI/UI/Xaml/Controls/Panel/Panel.cs
@@ -216,5 +216,7 @@ namespace Windows.UI.Xaml.Controls
 		partial void UpdateBorder();
 
 		internal override bool IsViewHit() => Border.IsViewHitImpl(this);
+
+		private protected UIElementCollection GetUnsortedChildren() => Children;
 	}
 }
